f0d8c2c48d410b4dd4a29e2df8ed5f68bee03b11
prathamsm7 <79472183+prathamsm7@users.noreply.github.com>
2022-05-24 20:52:43 +0530

    forgot password and reset password added

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -8,6 +8,8 @@ const jwt = require('jsonwebtoken');
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
+  port: 465,
+  secure: true,
   auth: {
     user: `${process.env.USER}`,
     pass: `${process.env.PASS}`,
